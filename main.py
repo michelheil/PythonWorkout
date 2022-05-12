58c871899bead859155be9385a97211dd526ef07
Michael Heil <michelheil@gmx.de>
2022-05-12 14:35:25 +0200

    Exercise 1 - Number guessing game

--- main.py
+++ main.py
@@ -2,14 +2,33 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def guessing_game():
+    # Exercise 1
+    # Number guessing game
+
+    # create random integer between 0 and 100
+    rand_int = random.randint(0, 100)
+
+    # compare guessed integer with random integer
+    while True:
+
+        # ask user to guess what number has been chosen
+        guessed_int = int(input('Enter your guess: '))
+
+        if guessed_int < rand_int:
+            print('Too low')
+
+        if guessed_int > rand_int:
+            print('Too high')
+
+        if guessed_int == rand_int:
+            print('Just right')
+            break
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    guessing_game()
